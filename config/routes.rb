89506fe733e6a3e5b9084da0a7dab5a90c9e05ad
Uduak Essien <acushla4real@gmail.com>
2021-01-11 11:49:27 +0100

    Update routes with required endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # default_url_options host: 'http://localhost:3000'
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: %i[index create destroy]
+
+      post 'authenticate', to: 'authentication#create'
+    end
+  end
 end

a6e762e1c3e60e79482067951583531ef12871ca
Uduak Essien <acushla4real@gmail.com>
2021-01-11 11:50:38 +0100

    Add book controller

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class BooksController < ApplicationController
+      MAX_PAGINATION_LIMIT = 100
+      before_action :authenticate_request!, only: :index
+
+      def index
+        books = Book.limit(limit).offset(params[:offset])
+
+        render json: BooksRepresenter.new(books).as_json
+      end
+
+      def create
+        author = Author.create!(author_params)
+        book = Book.new(book_params.merge(author_id: author.id))
+
+        if book.save
+          render json: BookRepresenter.new(book).as_json, status: :created
+        else
+          render json: book.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        Book.find(params[:id]).destroy!
+
+        head :no_content
+      end
+
+      private
+
+      def limit
+        [
+          params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i,
+          MAX_PAGINATION_LIMIT
+        ].min
+      end
+
+      def author_params
+        params.require(:author).permit(:first_name, :last_name, :age)
+      end
+
+      def book_params
+        params.require(:book).permit(:title)
+      end
+    end
+  end
+end

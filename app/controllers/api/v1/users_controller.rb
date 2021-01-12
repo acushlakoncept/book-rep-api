2ff2f15a2e691a840d72330d0a9fa5bd61a55d07
Uduak Essien <acushla4real@gmail.com>
2021-01-12 13:56:38 +0100

    Add user controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,24 @@
+module Api
+    module V1
+      class UsersController < ApplicationController
+        def create
+          user = User.create(user_params)
+
+          if user.save
+            render json: UserRepresenter.new(user).as_json, status: :created
+          else
+            render json: user.errors, status: :unprocessable_entity
+          end
+
+        end
+
+
+        private
+
+        def user_params
+          params.require(:user).permit(:username, :password)
+        end
+
+      end
+    end
+end

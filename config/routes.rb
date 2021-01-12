9b1767e527b3e5c4738a1bed5bc507be8d826ac9
Uduak Essien <acushla4real@gmail.com>
2021-01-12 13:57:27 +0100

    Update routes with register route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # default_url_options host: 'http://localhost:3000'
+  default_url_options host: 'http://localhost:3001'
   namespace :api do
     namespace :v1 do
       resources :books, only: %i[index create destroy]
 
       post 'authenticate', to: 'authentication#create'
+      post 'register', to: 'users#create'
     end
   end
 end
